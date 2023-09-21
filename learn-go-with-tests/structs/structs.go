e5785c8dba397debd84f876da848388f82cbb93b
Dudnyk, Anastasiia <Anastasiia.Dudnyk@viasat.com>
2023-09-21 15:43:25 +0200

    refactor(structs): use method to calculate area

--- learn-go-with-tests/structs/structs.go
+++ learn-go-with-tests/structs/structs.go
@@ -1,10 +1,29 @@
 package structs
 
+import "math"
+
 // A struct is a sequence of named elements, called fields, each of which has a name and a type.
 // Field names may be specified explicitly (IdentifierList) or implicitly (EmbeddedField).
 type Rectangle struct {
 	Width float64
 	Height float64
+}
+
+// method syntax:
+// func (receiverName ReceiverType) MethodName(args)
+
+// method of Rectangle struct
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+// method of Circle struct
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- learn-go-with-tests/structs/structs_test.go
+++ learn-go-with-tests/structs/structs_test.go
@@ -14,11 +14,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{5.0, 10.0}
-	got := Area(rectangle)
-	want := 50.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{5.0, 10.0}
+		got := rectangle.Area()
+		want := 50.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			// g will print a more precise decimal number in the error message
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
